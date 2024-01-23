0a92d94c73230f7962e473dd0e6c0e00605294cf
NagaJyothi <nagajyothi.bathula45@gmail.com>
2024-01-23 10:12:28 +0530

    modules commit

--- patient-billing-service/src/main/java/com/patient/billing/service/entity/PatientBillingEntity.java
+++ patient-billing-service/src/main/java/com/patient/billing/service/entity/PatientBillingEntity.java
@@ -3,6 +3,8 @@ package com.patient.billing.service.entity;
 public class PatientBillingEntity {
 	public static void main(String[] args) {
 		System.out.println("patient billing entity");
+		System.out.println("patient billing entity");
+
 	}
 
 }
