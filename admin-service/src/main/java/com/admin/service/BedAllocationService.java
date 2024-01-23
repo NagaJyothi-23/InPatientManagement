ec01264cebbb3f014a90cc965ce14548dd94df1e
padmalatha2001 <padmalathac2001@gmail.com>
2024-01-23 12:56:19 +0530

    crud done

---
+++ admin-service/src/main/java/com/admin/service/BedAllocationService.java
@@ -0,0 +1,14 @@
+package com.admin.service;
+
+import java.util.List;
+
+import com.admin.entity.BedAllocation;
+
+public interface BedAllocationService {
+
+	BedAllocation save(BedAllocation bedAllocation);
+	BedAllocation getById(long id);
+	List<BedAllocation> getAll();
+	void update(BedAllocation bedAllocation);
+	void delete(long id);
+}
