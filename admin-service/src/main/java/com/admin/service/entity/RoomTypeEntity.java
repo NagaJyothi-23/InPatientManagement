f00c884693f76ba69083b9a6607c158a1f420447
Rajeswari1598 <rajeswari1520@gmail.com>
2024-01-23 10:27:01 +0530

    commited

--- admin-service/src/main/java/com/admin/service/entity/RoomTypeEntity.java
+++ admin-service/src/main/java/com/admin/service/entity/RoomTypeEntity.java
@@ -3,6 +3,7 @@ package com.admin.service.entity;
 public class RoomTypeEntity {
 	public static void main(String[] args) {
 		System.out.println("room entity");
+		System.out.println("room entity");
 	}
 
 }
