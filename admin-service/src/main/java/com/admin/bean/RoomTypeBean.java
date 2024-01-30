8c39f6c9d6988b9175d9c4b7d43840bc262c5549
Rajeswari1598 <rajeswari1520@gmail.com>
2024-01-30 12:50:28 +0530

    done

---
+++ admin-service/src/main/java/com/admin/bean/RoomTypeBean.java
@@ -0,0 +1,72 @@
+package com.admin.bean;
+
+import com.admin.entity.Ward;
+
+public class RoomTypeBean {
+	private long roomType_id;
+	private String roomType_name;
+	private int roomSharing;
+	private double roomPrice;
+	private Ward ward_id;
+	
+	public RoomTypeBean() {
+		super();
+	}
+
+	public long getRoomType_id() {
+		return roomType_id;
+	}
+
+	public void setRoomType_id(long roomType_id) {
+		this.roomType_id = roomType_id;
+	}
+
+	public String getRoomType_name() {
+		return roomType_name;
+	}
+
+	public void setRoomType_name(String roomType_name) {
+		this.roomType_name = roomType_name;
+	}
+
+	public int getRoomSharing() {
+		return roomSharing;
+	}
+
+	public void setRoomSharing(int roomSharing) {
+		this.roomSharing = roomSharing;
+	}
+
+	public double getRoomPrice() {
+		return roomPrice;
+	}
+
+	public void setRoomPrice(double roomPrice) {
+		this.roomPrice = roomPrice;
+	}
+
+	public Ward getWard_id() {
+		return ward_id;
+	}
+
+	public void setWard_id(Ward ward_id) {
+		this.ward_id = ward_id;
+	}
+
+	public RoomTypeBean(long roomType_id, String roomType_name, int roomSharing, double roomPrice, Ward ward_id) {
+		super();
+		this.roomType_id = roomType_id;
+		this.roomType_name = roomType_name;
+		this.roomSharing = roomSharing;
+		this.roomPrice = roomPrice;
+		this.ward_id = ward_id;
+	}
+
+	@Override
+	public String toString() {
+		return "RoomTypeBean [roomType_id=" + roomType_id + ", roomType_name=" + roomType_name + ", roomSharing="
+				+ roomSharing + ", roomPrice=" + roomPrice + ", ward_id=" + ward_id + "]";
+	}
+	
+	
+}
--- admin-service/src/main/java/com/admin/service/implementation/RoomTypeServiceImplementation.java
+++ admin-service/src/main/java/com/admin/service/implementation/RoomTypeServiceImplementation.java
@@ -5,15 +5,27 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.admin.bean.RoomTypeBean;
 import com.admin.entity.RoomTypeEntity;
 import com.admin.exception.RecordNotFoundException;
 import com.admin.repository.RoomTypeRepository;
 import com.admin.service.RoomTypeService;
+
 @Service
-public class RoomTypeServiceImplementation implements RoomTypeService{
+public class RoomTypeServiceImplementation implements RoomTypeService {
 
 	@Autowired
 	RoomTypeRepository roomTypeRepository;
+
+	public RoomTypeEntity convertBeantoEntity(RoomTypeBean bean) {
+		RoomTypeEntity entity = new RoomTypeEntity();
+		entity.setRoomType_id(bean.getRoomType_id());
+		entity.setRoomType_name(bean.getRoomType_name());
+		entity.setRoomSharing(bean.getRoomSharing());
+		entity.setRoomPrice(bean.getRoomPrice());
+		entity.setWard_id(bean.getWard_id());
+		return entity;
+	}
 
 	@Override
 	public void save(RoomTypeEntity roomType) {
@@ -24,13 +36,23 @@ public class RoomTypeServiceImplementation implements RoomTypeService{
 	public List<RoomTypeEntity> getAll() {
 		List<RoomTypeEntity> roomlist = roomTypeRepository.findAll();
 		return roomlist;
-		
+
 	}
 
 	@Override
 	public RoomTypeEntity getById(long id) {
-		return roomTypeRepository.findById(id).orElseThrow(()->
-		new RecordNotFoundException("Room not found with id"+id));		
+		return roomTypeRepository.findById(id)
+				.orElseThrow(() -> new RecordNotFoundException("Room not found with id" + id));
+	}
+
+	public RoomTypeBean convertEntitytoBean(RoomTypeEntity entity) {
+		RoomTypeBean bean = new RoomTypeBean();
+		bean.setRoomType_id(entity.getRoomType_id());
+		bean.setRoomType_name(entity.getRoomType_name());
+		bean.setRoomSharing(entity.getRoomSharing());
+		bean.setRoomPrice(entity.getRoomPrice());
+		bean.setWard_id(entity.getWard_id());
+		return bean;
 	}
 
 	@Override
@@ -42,7 +64,7 @@ public class RoomTypeServiceImplementation implements RoomTypeService{
 	@Override
 	public void delete(long id) {
 		roomTypeRepository.deleteById(id);
-		
+
 	}
 
 }
