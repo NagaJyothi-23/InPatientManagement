7a35452dad0ce3acfc5b0e0a956cc6b6d7f8d1ab
AleKeerthi <alekeerthi13@gmail.com>
2024-01-31 10:33:42 +0530

    patient service

--- patient-service/src/main/java/com/patient/exception/PatientIdNotFoundException.java
+++ patient-service/src/main/java/com/patient/exception/PatientIdNotFoundException.java
@@ -2,6 +2,11 @@ package com.patient.exception;
 
 public class PatientIdNotFoundException extends RuntimeException {
 	
+	public PatientIdNotFoundException() {
+	super();
+	}
+	
+	
 	public PatientIdNotFoundException(String message) {
 		super(message);
 	}
