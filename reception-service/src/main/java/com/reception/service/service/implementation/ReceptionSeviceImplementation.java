ff71de15472c536a6e261609a1c462e2c15675bd
NagaJyothi <nagajyothi.bathula45@gmail.com>
2024-02-01 08:53:16 +0530

    commited

--- reception-service/src/main/java/com/reception/service/service/implementation/ReceptionSeviceImplementation.java
+++ reception-service/src/main/java/com/reception/service/service/implementation/ReceptionSeviceImplementation.java
@@ -1,10 +1,4 @@
 package com.reception.service.service.implementation;
-
-
-
-
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -13,9 +7,9 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
-
+import com.reception.service.bean.BillingBean;
 import com.reception.service.bean.PatitentBean;
+import com.reception.service.exception.PatitentDetailsNotFoundException;
 import com.reception.service.service.ReceptionService;
 @Service
 public class ReceptionSeviceImplementation implements ReceptionService
@@ -24,18 +18,41 @@ public class ReceptionSeviceImplementation implements ReceptionService
 	private RestTemplate restTemplate;
 
 	@Override
-	public PatitentBean patitentDetails(int id) {
-		String url = "http://localhost:8080/registration/"+id;
+	public PatitentBean patitentDetails(int patitentid) {
+		String url = "http://localhost:8082/billing/fetch/"+patitentid;
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		HttpEntity<String> httpEntity = new HttpEntity<>(headers);
+		if(httpEntity!=null) {
+		ResponseEntity<PatitentBean> responseEntity = restTemplate.exchange(url, HttpMethod.GET, httpEntity,
+				PatitentBean.class);
+		PatitentBean patitentBean = responseEntity.getBody();
+		return patitentBean;
+		}
+		else
+		{
+			throw new PatitentDetailsNotFoundException("Patitent Details Not found");
+		}
+	}
+
+	@Override
+	public BillingBean billingDetails(int billId) {
+		
+		String url = "http://localhost:8082/billing/fetch/"+billId;
 
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentType(MediaType.APPLICATION_JSON);
 		HttpEntity<String> httpEntity = new HttpEntity<>(headers);
 
-		ResponseEntity<PatitentBean> responseEntity = restTemplate.exchange(url, HttpMethod.GET, httpEntity,
-				PatitentBean.class);
-		PatitentBean patitentBean = responseEntity.getBody();
-		return patitentBean;
+		ResponseEntity<BillingBean> responseEntity = restTemplate.exchange(url, HttpMethod.GET, httpEntity,
+				BillingBean.class);
+		BillingBean billingBean = responseEntity.getBody();
+		return billingBean;
+		
+		
 	}
+	
 	
 
 }
